f1ad7477927db683166800aa50504353ceee0dd1
Teymur <teymur_gahramanov@gmail.com>
2022-11-27 22:39:00 +0400

    minor changes

--- tests/functional/test_home.py
+++ tests/functional/test_home.py
@@ -5,4 +5,4 @@ def test_home():
   with flask_app.test_client() as test_client:
       response = test_client.get('/')
       assert response.status_code == 200
-      assert b"dummy Web application and API" in response.data
+      assert b"App22" in response.data

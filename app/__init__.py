2d841ff4958a561c4442a1c908fb7fe14992a0eb
Teymur <teymur_gahramanov@gmail.com>
2024-03-15 00:12:03 +0400

    upd

--- app/__init__.py
+++ app/__init__.py
@@ -14,6 +14,11 @@ def load_config(config):
 def create_app() :
   app = Flask(__name__, instance_relative_config=True)
 
+  SECRET_KEY = os.urandom(24)
+  SQLALCHEMY_DATABASE_URI = f'{db_uri}'
+  SQLALCHEMY_TRACK_MODIFICATIONS = False
+  SQLALCHEMY_ENGINE_OPTIONS = {'echo_pool':'debug',"connect_args": {'sslmode':"disable"}}
+  
   with app.app_context():
     PrometheusMetrics(app)
     from . import routes
